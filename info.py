7c1b0da4de491503c8b2574d9a130aeef2ec8c4a
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-06-24 21:10:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -101,7 +101,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False))
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Mdisk.pro')
 SHORTLINK_API = environ.get('SHORTLINK_API', '766185149277536bd3b9bbf1ccc75cb97a411729')
 
